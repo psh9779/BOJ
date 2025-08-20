5139009625190bad249fdb5878f2d0da6bfd70fc
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-08-21 00:38:41 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -2,15 +2,20 @@
 using namespace std;
 
 int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
 	int N, X, seq[10001];
 
 	cin >> N >> X;
+	// cin과 cout이 반복문 안에서 번갈아 발생
+	// tie를 끊어 (cin.tie(NULL)) cin 입력 시 자동 flush가 발생하지 않게 하여 속도를 향상시킴
 	for (int i = 0; i < N; i++) {
 		cin >> seq[i];
 		if (seq[i] < X)
 			cout << seq[i] << ' ';
 	}
-	cout << endl;
+	cout << endl;    // 반복문 중간에 flush가 발생하지 않았더라도, endl에 의해 버퍼가 flush되어 이 시점에 화면에 출력
 
 	return 0;
 }
