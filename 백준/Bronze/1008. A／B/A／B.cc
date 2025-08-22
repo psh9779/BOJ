a42cfec204718729fa4c695fa10a5cfb4192ea2a
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-08-22 21:06:21 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -9,10 +9,9 @@ int main(void) {
 	cin >> A >> B;
 
 	cout << fixed;    // fixed + precision(n) : 소수점 n자리까지 반올림(고정)하여 출력
-	cout.precision(15);    // 상대오차가 10^-9 이하이면 정답이므로, 최소 출력 자릿수는 10자리 이상임 (상대오차가 10^-x 이하이면 정답이다 == 최소 소수점 x+1자리 이상은 출력해야 한다)
+	cout.precision(15);    // 상대오차가 10^-9 이하이면 정답이므로, 충분히 큰 precision으로 출력을 안전하게 하도록 함
 	
 	cout << double(A) / B;
 
 	return 0;
-
 }
