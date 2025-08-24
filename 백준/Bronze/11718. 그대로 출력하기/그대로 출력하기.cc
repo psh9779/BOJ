df945b1afabb4b2aa7f226cffe5ae68d415fc0bd
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-08-25 03:47:07 +0900

    [Bronze III] Title: 그대로 출력하기, Time : 0 ms, Memory : 2024 KB - BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -9,7 +9,13 @@ int main(void) {
 
 	string s;
 	for (int i = 0; i < 100; i++) {    // 입력은 최대 100줄
-		if (!getline(cin, s)) break;    // EOF가 입력되면(Window에서는 ctrl+z) getline() 반환값은 false.
+		/*
+		공백 포함 문자열을 입력->getline()
+		EOF가 입력 시 (Window에서는 ctrl+z + enter) getline() 반환값은 false
+		Windows 콘솔은 라인 버퍼 모드로 동작하므로 ctrl + z만으로는 EOF가 전달되지 않고,
+		Enter를 눌러야 프로그램이 EOF를 인식함
+		*/
+		if (!getline(cin, s)) break;
 		cout << s << '\n';
 	}
 
