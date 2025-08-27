48af55e82884b2b189bb4b4f349807a45d88b1ab
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-08-27 21:49:06 +0900

    [Bronze V] Title: UCPC에서 가장 쉬운 문제 번호는?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25311. UCPC에서 가장 쉬운 문제 번호는？/UCPC에서 가장 쉬운 문제 번호는？.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	// 가장 쉬운 문제의 위치를 고정하므로 "A번이 고정되어있음"
+	// 따라서, 연도별로 출제진이 의도한 가장 쉬운 문제의 번호는 'A'를 출력
+	int y;
+	cin >> y;
+	cout << 'A';
+
+	return 0;
+}
