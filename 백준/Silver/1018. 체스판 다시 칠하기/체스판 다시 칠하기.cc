8fffcb9e4ee2a51c5d20738c4bd076fd595af643
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-09-01 01:04:20 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time : 0 ms, Memory : 2020 KB - BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -10,6 +10,7 @@ int main() {
 	
 	char start_white[50][50], start_black[50][50], input[50][50];
 
+	/*    < 결국 필요가 없어진 코드 >
 	// 맨 왼쪽 위 칸이 흰색, 검은색인 8 x 8 체스판 초기화
 	for (int row = 0; row < 8; row++) {
 		for (int col = 0; col < 8; col++) {
@@ -23,8 +24,9 @@ int main() {
 			}
 		}
 	}
+	*/
 
-	// 입력으로 주어지는 보드의 각 행의 상태 저장
+	// 입력으로 주어지는 보드를 각각 행단위로 상태 저장
 	for (int row = 0; row < N; row++)
 		cin >> input[row];
 
@@ -62,3 +64,54 @@ int main() {
 	
 	return 0;
 }
+
+
+
+ /* 깔끔하게 작성한 코드 (99 ~ 107번 줄 중요)
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M;    // (N*M == 행개수 * 열개수) 크기의 보드판
+	cin >> N >> M;
+
+	string board[50];    // 각 요소 board[cnt]가 std::string 객체임
+	for (int i = 0; i < N; i++)
+		cin >> board[i];
+
+	int result = 64;
+
+	// 모든 시작 위치 탐색
+	for (int i = 0; i <= N - 8; i++) {
+		for (int j = 0; j <= M - 8; j++) {
+			int cnt_w = 0;    // 왼쪽 위가 White일 때, 다시 칠해야 할 정사각형 개수
+			int cnt_b = 0;    // 왼쪽 위가 Black일 때, 다시 칠해야 할 정사각형 개수
+
+			for (int x = 0; x < 8; x++) {
+				for (int y = 0; y < 8; y++) {
+					char cur = board[i + x][j + y];
+					
+					// x+y가 짝수면 8*8 보드의 왼쪽 위와 같은 색깔이어야 하며, 홀수이면 8*8 보드의 왼쪽 위와 다른 색깔이어야 한다.
+					if ((x + y) % 2 == 0) {
+						if (cur != 'W') cnt_w++;    // 왼쪽 위가 White인 체스판을 기준으로 생각했을 때, 다시 칠해야 할 정사각형 개수 (인덱스 합이 짝수인 경우엔 W이어야 하므로)
+						if (cur != 'B') cnt_b++;    // 왼쪽 위가 Black인 체스판을 기준으로 생각했을 때, 다시 칠해야 할 정사각형 개수
+					}
+					else {
+						if (cur != 'B') cnt_w++;    // 왼쪽 위가 White인 체스판을 기준으로 생각했을 때, 다시 칠해야 할 정사각형 개수
+						if (cur != 'W') cnt_b++;    // 왼쪽 위가 Black인 체스판을 기준으로 생각했을 때, 다시 칠해야 할 정사각형 개수
+					}
+				}
+			}
+			result = min(result, min(cnt_w, cnt_b));    // result와 (cnt_w, cnt_b 중의 최솟값)의 최솟값을 계산
+		}
+	}
+	cout << result << '\n';
+
+	return 0;
+}
+*/
