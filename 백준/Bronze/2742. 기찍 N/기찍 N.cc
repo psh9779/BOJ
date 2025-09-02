40db76433b4129059633cb0ca770b160a93c7a70
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-09-02 23:00:35 +0900

    [Bronze V] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	for (int cnt = N; cnt > 0; cnt--)
+		cout << cnt << '\n';
+
+	return 0;
+}	
