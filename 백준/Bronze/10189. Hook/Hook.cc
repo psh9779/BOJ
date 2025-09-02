a8a1791130ffd912d0e22ab2e94d29e5abc98fd5
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-09-02 23:12:04 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	cout << "#  # #### #### #  #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#### #  # #  # # #\n";
+	cout << "#  # #### #### #  #\n";
+	
+	return 0;
+}	
