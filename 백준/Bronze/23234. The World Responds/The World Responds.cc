96adaa8fafe0d82740e37cb281ad9416630f703d
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-09-05 00:10:37 +0900

    [Bronze V] Title: The World Responds, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/23234. The World Responds/The World Responds.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// "Hello, World!"를 출력하는 프로그램은 세상에 인사를 하지만, 세상은 응답하지 않음
+// 따라서, 세상(world)이 프로그램에게 인사를 돌려주도록 세상에서 프로그램에게 인사를 보내는 프로그램을 작성할 것
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout << "The world says hello!\n";
+
+	return 0;
+}
