aa16399f081f5530e91af170c3e406c894b541e7
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-09-06 14:22:08 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -16,16 +16,45 @@ int main() {
 	int N;
 	cin >> N;
 
-	vector<vector<int>> arr(N, vector<int>(2));
+	vector<pair<int, int>> arr(N);
 	for (int cnt = 0; cnt < N; cnt++)
-		cin >> arr[cnt][0] >> arr[cnt][1];
+		cin >> arr[cnt].first >> arr[cnt].second;
 
 	// vector<pair<int,int>> arr;를 정렬하면, x 기준으로 정렬, x가 같으면 y기준으로 자동 정렬
 	// 단, y좌표를 우선으로 정렬하려면 비교 함수를 직접 넘겨줘야 함
 	sort(arr.begin(), arr.end());
 
 	for (int cnt = 0; cnt < N; cnt++)
-		cout << arr[cnt][0] << ' ' << arr[cnt][1] << '\n';
+		cout << arr[cnt].first << ' ' << arr[cnt].second << '\n';
 
 	return 0;
 }
+
+/* 2차원 벡터를 사용하는 방식
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	vector<vector<int>> arr(N, vector<int>(2));
+	for (int i = 0; i < N; i++)
+		cin >> arr[i][0] >> arr[i][1];
+
+	sort(arr.begin(), arr.end(), [](auto& a, auto& b) {    // a와 b는 2차원 벡터
+		if (a[0] == b[0]) return a[1] < b[1];
+		return a[0] < b[0];
+		});
+
+	for (int j = 0; j < N; j++)
+		cout << arr[j][0] << ' ' << arr[j][1] << '\n';
+
+	return 0;
+}
+*/
