026f41e99c374095abce05f2197e37797cc4621f
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-09-13 02:18:19 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, K;
+	cin >> N >> K;
+
+	vector<int> coin_value(N);
+	for (int i = 0; i < N; i++)
+		cin >> coin_value[i];
+
+	int min_coin_cnt = 0;
+	for (int j = N - 1; j >= 0; j--) {    // K == 0이면 조기 종료가 가능함
+		min_coin_cnt += K / coin_value[j];
+		K %= coin_value[j];
+	}
+	cout << min_coin_cnt;
+	
+	return 0;
+}
