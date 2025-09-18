7936dc6b09fd5dddd010e1edeb7b69450e57b86c
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-09-18 22:08:54 +0900

    [Silver V] Title: 막대기, Time : 0 ms, Memory : 2020 KB - BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.cc
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -9,7 +9,7 @@ int main() {
 	cin >> X;    // 1 <= X <= 64
 
 	int stick_len = 64, cnt = 0;
-	while (X){
+	while (X) {
 		if (X >= stick_len) {
 			X -= stick_len;
 			cnt++;
@@ -20,3 +20,40 @@ int main() {
 
 	return 0;
 }
+
+
+
+/* 이진 탐색을 이용한 풀이
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int stick_len = 64;
+	int X;
+	cin >> X;
+
+	int low = 0, high = stick_len, cnt = 0;
+
+	// 현재 문제 상황에서, 타겟 X는 무조건 찾는 값임
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		if (mid < X) {
+			low = mid + 1;
+			cnt++;    // 이때만 ++연산
+		}
+		else if (mid > X) {
+			high = mid;
+		}
+		else {    // mid == X. 즉, 타겟 X를 찾은 경우
+			cnt++;
+			break;
+		}
+	}
+	cout << cnt << '\n';
+
+	return 0;
+}
+*/
