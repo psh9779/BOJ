61df57c3fb601ae2c3fa64c1a448dd79268910e1
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-09-23 02:13:02 +0900

    [Silver V] Title: 그룹 단어 체커, Time : 0 ms, Memory : 2024 KB - BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.cc
@@ -1,33 +1,41 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-int main(void) {
+int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	int N;    // 100 이하의 자연수
-	string s;
-
+	int N;
 	cin >> N;
-	int cnt = 0;
 
-	for (int i = 0; i < N; i++) {
-		bool isdup = false;
-		cin >> s;
-		
-		for (int j = 0; j < s.length(); j++) {
-			for (int k = j + 1; k < s.length() - 1; k++)
-				if (s[j] != s[k] && s[j] == s[k + 1]) {
-					if (!isdup) {
-						isdup = true;
-						break;
-					}
+	int cnt = 0;    // 그룹 단어 개수
+
+	while (N--) {
+		bool alpha[26] = { 0 };    // 모든 요소 false 초기화
+		bool isGroupWord = true;    // 각 반복마다 그룹단어인지 판단할 플래그
+
+		string word;
+		cin >> word;
+
+		char prev = word[0];
+		alpha[word[0] - 'a'] = 1;
+
+		for (int i = 1; i < word.length(); i++) {
+			// 이전 문자와 달라진 경우만 확인
+			if (word[i] != prev) {
+				// 이전에 나온 문자가 다시 나온 경우
+				if (alpha[word[i] - 'a'] != 0) {
+					isGroupWord = false;
+					break;
 				}
-			if (isdup) break;
+
+				// 이전에 나온 문자가 아닌 다른 문자가 나온 경우
+				alpha[word[i] - 'a'] = 1;
+				prev = word[i];
+			}
 		}
-		if (!isdup) cnt++;
+		if (isGroupWord) cnt++;
 	}
 	cout << cnt << '\n';
 
