ce0bd7b9c8810f199f78676781fab150c0e9e24d
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-09-28 23:02:12 +0900

    [Bronze III] Title: 플러그, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL)
+using namespace std;
+
+int main() {
+	fastio;
+
+	int N;
+	cin >> N;
+
+	int ans = 0;
+	for (int i = 0; i < N; i++) {
+		int temp;
+		cin >> temp;
+
+		if (i == 0)
+			ans += temp;    // 처음 멀티탭을 꽂는 경우
+		else
+			ans += (temp - 1);    // 두번째부터 멀티탭을 꽂는 경우 (콘센트 하나는 다른 멀티탭 연결을 위해 쓰이므로)
+	}
+
+	cout << ans << '\n';
+
+	return 0;
+}
