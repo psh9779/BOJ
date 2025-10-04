2eb95576156bc3be6bf4a3fd8492024941b4f900
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-10-05 03:24:46 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -1,23 +1,25 @@
+// 큰 수의 제곱 -> divide and conquer 방식
 #include <iostream>
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL);
 using namespace std;
 
-// (a ^ b) % c == (a%c * a%c * a%c ... * a%c) % c
-// (8^4) % c == (64^2) % c == (4^6) % c == (2^12) % c
-long long div(long long A, long long B, long long C) {
+long long A, B, C;
+
+long long power(long long B) {
+	if (B == 0) return 1;
 	if (B == 1) return A % C;
-	if (B % 2) return (div(A, B / 2, C) * div(A, B / 2, C) % C * A) % C;
-	else return div(A, B / 2, C) * div(A, B / 2, C) % C;
+
+	long long K = power(B / 2) % C;
+	if (B % 2) return (K * K % C * A) % C;
+	else return K * K % C;
 }
 
 int main() {
 	fastio;
-
-	long long A, B, C;
 	cin >> A >> B >> C;
 
-	long long res = 1;
-	res = div(A, B, C);
+	long long res;
+	res = power(B);
 
 	cout << res << '\n';
 
