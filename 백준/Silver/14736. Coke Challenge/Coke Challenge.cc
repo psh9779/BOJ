7bcd2331f670096242abb662a7eb6e9ad46ef4c4
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-10-05 00:14:04 +0900

    [Silver V] Title: Coke Challenge, Time : 0 ms, Memory : 2020 KB - BaekjoonHub

--- 백준/Silver/14736. Coke Challenge/Coke Challenge.cc
+++ 백준/Silver/14736. Coke Challenge/Coke Challenge.cc
@@ -26,3 +26,46 @@ int main(){
 
 	return 0;
 }
+
+
+
+
+
+/* 수학적 방식으로 풀이 (올림 처리)
+#include <iostream>
+#include <climits>    // LLONG_MAX
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+using namespace std;
+
+int main() {
+	fastio;
+
+	ll N, K, A;
+	cin >> N >> K >> A;
+
+	long long min_time = LLONG_MAX;
+
+	for (int i = 0; i < N; i++) {
+		ll ti, si;
+		cin >> ti >> si;
+
+		ll unit_dist = ti * A;    // 한 번 달리고 쉬는 단위 거리
+
+		ll repeat = K / unit_dist;    // 반복 횟수 (ti와 si를 모두 거치는)
+
+		ll rest = K % unit_dist;    // 남은 거리
+
+		ll total = repeat * (ti + si) + (rest + A - 1) / A;    // 총 시간 == 완전 반복 시간 + 남은 거리 이동 시간(올림처리)
+
+		if (rest == 0)
+			total -= si;    // 남은 거리가 0인 경우에는 마지막 쉬는 시간 제거
+
+		if (total < min_time)    // 최솟값 갱신
+			min_time = total;
+	}
+
+	cout << min_time << '\n';
+	return 0;
+}
+*/
