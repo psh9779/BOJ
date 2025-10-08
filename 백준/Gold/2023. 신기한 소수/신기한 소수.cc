5ec3662b148fc5d017d9f5885328f7a0c497562a
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-10-09 01:51:14 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.cc
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -1,46 +1,40 @@
 #include <iostream>
-#include <string>
-#include <deque>
-#include <utility>    // std::swap()
 #define fastio ios::sync_with_stdio(false); cin.tie(NULL)
 using namespace std;
 
+int N;
 bool isPrime(int n) {
-	if (n % 2 == 0 || n % 3 == 0)
-		return false;
-	for (int i = 5; i * i <= n; i += 6)
-		if (n % i == 0 || n % (i + 2) == 0)
+	for (int i = 2; i * i <= n; i++) {
+		if (n % i == 0)
 			return false;
+	}
 	return true;
 }
 
-// 2,3,5,7로 시작하는 수만 확인하면 됨
+void DFS(int n, int jarisu) {
+	// 한자리 수의 경우, 바로 출력하고 소수판별은 하지 않음
+	if (N == jarisu) {
+		cout << n << '\n';
+		return;
+	}
+	
+	// 인자로 전달한 jarisu 값이 입력으로 받은 자릿수와 다른 경우
+	for(int i = 1; i < 10; i += 2){    // 일의 자리수가 홀수인 경우만 확인
+		if (isPrime(n * 10 + i))
+			DFS(n * 10 + i, jarisu + 1);    //  자릿수를 증가시켜서 DFS 재귀 호출
+	}
+	
+	return;
+}
+
 int main() {
 	fastio;
-	int N; cin >> N;
+	cin >> N;
 
-	deque<string> dq = { "2","3","5","7" };
-	int size = 1;
-	while (size != N) {
-		deque<string> tmp_dq;
-		while (!dq.empty()) {
-			string num = dq.front();
-			for (int i = 1; i < 10; i += 2) {
-				string temp;
-				temp = num + to_string(i);
-				if (isPrime(stoi(temp)))
-					tmp_dq.push_back(temp);
-			}
-			dq.pop_front();
-		}
-		swap(dq, tmp_dq);    // swap()으로 내부 포인터만 교환 (O(1))
-		size++;
-	}
-
-	while (!dq.empty()) {
-		cout << dq.front() << '\n';
-		dq.pop_front();
-	}
+	DFS(2, 1);
+	DFS(3, 1);
+	DFS(5, 1);
+	DFS(7, 1);
 
 	return 0;
 }
