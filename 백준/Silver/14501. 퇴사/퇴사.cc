9b5158563d58da53cc694aa931554940aaad8f9a
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-10-09 15:49:41 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -5,35 +5,32 @@
 using namespace std;
 
 int N, max_profit = 0;
-vector<pair<int, int>> v;
+vector<int> T, P;
 
-void DFS(int day, int cur_profit) {    // 시작일, 현재까지의 이익
-	if (day == N + 1) {
-		max_profit = max(max_profit, cur_profit);
-		return;
-	}
+void BruteForce(int day, int cur_profit) {    // 시작일, 현재까지의 이익
+	// 상담 날짜가 퇴사일을 넘으면 종료
+	if (day > N + 1) return;
 
-	if (day > N + 1)    // 퇴사일이 넘으면 종료
-		return;
-
-	// 오늘부터 상담을 시작하는 경우
-	if (day + v[day].first <= N + 1)
-		DFS(day + v[day].first, cur_profit + v[day].second);
+	max_profit = max(max_profit, cur_profit);
 	
-	// 오늘부터 상담을 시작하지 않는 경우
-	DFS(day + 1, cur_profit);
+	for (int i = day; i <= N; i++) {
+		if (i + T[i] <= N + 1) {
+			BruteForce(i + T[i], cur_profit + P[i]);
+		}
+	}
 }
 
 int main() {
 	fastio;
 
 	cin >> N;
-	v.resize(N + 1);
+	T.resize(N + 1);
+	P.resize(N + 1);
 
 	for (int i = 1; i <= N; i++)
-		cin >> v[i].first >> v[i].second;
+		cin >> T[i] >> P[i];
 
-	DFS(1, 0);
+	BruteForce(1, 0);    // 1일부터 시작
 
 	cout << max_profit << '\n';
 
