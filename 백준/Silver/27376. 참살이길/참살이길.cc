3e2ea61a0d214067ceb28d5b40b39faafaa6c5c1
sanghyeon <90553124+psh9779@users.noreply.github.com>
2025-10-12 22:15:34 +0900

    [Silver III] Title: 참살이길, Time: 72 ms, Memory: 8360 KB -BaekjoonHub

---
+++ 백준/Silver/27376. 참살이길/참살이길.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+#define fastio ios::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+using namespace std;
+
+int main() {
+	fastio;
+
+	ll n, k; cin >> n >> k;
+	map<ll, pair<ll, ll>> mp;
+	
+	for (int i = 0; i < k; i++) {
+		ll x, t, s; cin >> x >> t >> s;
+		mp[x] = { t,s };
+	}
+
+	ll total_t = 0;
+	ll before_pos = 0, next_pos;
+
+	// p.first는 key, p.second는 value에 해당함
+	for (auto& p : mp) {
+		next_pos = p.first;
+		ll t = p.second.first, s = p.second.second;
+
+		total_t += next_pos - before_pos;
+		
+		if (total_t < s)
+			total_t = s;
+		else {
+			ll temp = (total_t - s) % (2 * t);
+
+			// 빨간불 상태인 경우
+			if (temp >= t)
+				total_t += (2 * t - temp);
+		}
+
+		before_pos = next_pos;
+	}
+	cout << total_t + (n - before_pos) << '\n';
+
+	return 0;
+}
