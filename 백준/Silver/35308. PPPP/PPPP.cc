ce770c8585b3e41c28cc2698c71cc982726bb26a
sanghyeon <90553124+psh9779@users.noreply.github.com>
2026-03-23 22:19:35 +0900

    [Silver IV] Title: PPPP, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/35308. PPPP/PPPP.cc
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+using namespace std;
+
+// K값이 정해지므로 만족하는 수열은 테스트케이스마다 정확히 한개이거나, 없을 수 있음
+// N개의 정수를 한번씩만 사용하여 다음 조건을 만족하는 수열을 만드려면
+// P_i = i+1을 만족하는 경우만 겹치지 않게 수열 생성 가능
+
+int main(){
+    fastio;
+    int T;
+    cin >> T;
+
+    while(T--){
+        int N,K;
+        cin >> N >> K;
+
+        string res;
+
+        if(N == 1){
+            cout << K << '\n';
+            continue;
+        }
+        else if(K != 2){
+            cout << "-1\n";
+            continue;
+        }
+        
+        for(int i = 1; i <= N; i++){
+            if(i == 1){
+                cout << i%N + 1;
+                continue;
+            }
+            cout << ' ' << i%N + 1;
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
