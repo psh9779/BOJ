8c322afd2c71ab306f9f15a115f8475fdfdf1352
sanghyeon <90553124+psh9779@users.noreply.github.com>
2026-03-25 00:43:30 +0900

    [Bronze V] Title: 역사를 걸으면 동국이 보이고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34721. 역사를 걸으면 동국이 보이고/역사를 걸으면 동국이 보이고.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+using namespace std;
+
+int main(){
+    fastio;
+
+    int N; cin >> N;
+    while(N--){
+        cout << "I love DGU\n";
+    }
+
+    return 0;
+}
