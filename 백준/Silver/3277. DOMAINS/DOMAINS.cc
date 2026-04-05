85c30b19f6fc4ba0e21d5cccb5c7a6a7033ee174
sanghyeon <90553124+psh9779@users.noreply.github.com>
2026-04-05 14:18:42 +0900

    [Silver IV] Title: DOMAINS, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/3277. DOMAINS/DOMAINS.cc
+++ 백준/Silver/3277. DOMAINS/DOMAINS.cc
@@ -2,26 +2,26 @@
 #define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
 using namespace std;
 
-// http://라는 접두어로 시작할 수 있으며
-// .으로 구분되는 단어들이 올 수 있다. (이 부분이 컴퓨터의 이름이다.)
-// 그 이후로 /로 구분되는 여러 단어들이 올 수 있는데 이는 경로이다.
-// 도메인 이름은 컴퓨터 이름의 가장 마지막 단어이다. (이 부분이 핵심)
+// 1. http://라는 접두어로 시작 가능 (없을 수 있음)
+// 2. 이후 computer name이 나타나는데, 두개 이상의 이름은 .으로 구분됨
+// 3. computer name이후 경로가 /를 구분자로하여 나타날 수 있음
+// 4. domain name은 computer name의 마지막 단어임
 string getDomain(string s){
     const string prefix = "http://";
-    if(s.substr(0, prefix.size()) == prefix){
+    if(prefix == s.substr(0, prefix.size())){
         s = s.substr(prefix.size());
     }
 
-    size_t slash_pos = s.find('/');    // 컴퓨터 이름 뒤 경로 제거
-    if(slash_pos != string::npos){
-        s = s.substr(0, slash_pos);
+    auto slashpos = s.find('/');
+    if(slashpos != string::npos){
+        s = s.substr(0, slashpos);
     }
 
-    size_t domain_pos = s.rfind('.');
-    if(domain_pos != string::npos){
-        s = s.substr(domain_pos+1);
+    auto lastdot = s.rfind('.');
+    if(lastdot != string::npos){
+        s = s.substr(lastdot + 1);
     }
-    
+
     return s;
 }
 
@@ -30,28 +30,33 @@ int main(){
 
     int N;
     cin >> N;
-
-    map<string, int> mp;
+    
+    unordered_map<string, int> mp;
     while(N--){
         string addr;
         cin >> addr;
-        
+
         string domain = getDomain(addr);
         mp[domain]++;
     }
 
-    int max_v = 0;
+    int maxn = 0;
     for(auto &p : mp){
-        max_v = max(max_v, p.second);
+        maxn = max(maxn, p.second);
     }
 
-    cout << max_v << '\n';
-
+    cout << maxn << '\n';
+    bool isfirst = true;
     for(auto &p : mp){
-        if(p.second == max_v){
-            cout << p.first << ' ';
+        if(p.second == maxn){
+            if(isfirst){
+                cout << p.first;
+                isfirst = false;
+                continue;
+            }
+            cout << ' ' << p.first;
         }
     }
-
+    cout << '\n';
     return 0;
 }
